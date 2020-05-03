fdc55aa5ca2b2cc7a58fbf0e77bc418d2c56e248
Andreas Glatz <glatzan@gmail.com>
2020-05-03 20:43:49 +0200

    Initial commit

---
+++ src/main/kotlin/de/sepdetect/sepdetect/SepdetectApplication.kt
@@ -0,0 +1,11 @@
+package de.sepdetect.sepdetect
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SepdetectApplication
+
+fun main(args: Array<String>) {
+    runApplication<SepdetectApplication>(*args)
+}
---
+++ src/test/kotlin/de/sepdetect/sepdetect/SepdetectApplicationTests.kt
@@ -0,0 +1,13 @@
+package de.sepdetect.sepdetect
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SepdetectApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
