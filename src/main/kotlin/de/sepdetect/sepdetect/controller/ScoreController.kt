7038645023dc1de52239f49d687deb185ee70777
Andreas Glatz <glatzan@gmail.com>
2020-05-09 22:27:58 +0200

    ScoreController

--- src/main/kotlin/de/sepdetect/sepdetect/controller/ScoreController.kt
+++ src/main/kotlin/de/sepdetect/sepdetect/controller/ScoreController.kt
@@ -1,22 +1,40 @@
 package de.sepdetect.sepdetect.controller
 
-import de.sepdetect.sepdetect.model.Patient
 import de.sepdetect.sepdetect.model.Score
+import de.sepdetect.sepdetect.model.ScoreValue
+import de.sepdetect.sepdetect.repository.PatientRepository
 import de.sepdetect.sepdetect.repository.ScoreRepository
 import org.springframework.web.bind.annotation.*
+import java.time.LocalDate
 import java.util.*
+import javax.persistence.EntityNotFoundException
 
 @RestController
 class ScoreController constructor(
-        private val scoreRepository: ScoreRepository) {
+        private val scoreRepository: ScoreRepository,
+        private val patientRepository: PatientRepository) {
 
     @GetMapping("scores/{patientID}")
-    fun getScores(@PathVariable patientID: Long) : List<Score> {
+    fun getScores(@PathVariable patientID: Long): List<Score> {
         return scoreRepository.findAllByPatientPersonIdOrderByListOrder(patientID)
     }
 
     @PostMapping("score/add/{patienID}")
-    fun addScore(@PathVariable patientID: Long, @RequestBody patient: Patient, @RequestParam("new") active: Optional<Boolean>) {
+    fun addScore(@PathVariable patientID: Long, @RequestBody scoreValue: ScoreValue, @RequestParam("newScore") newScore: Optional<Boolean>) {
+        val patient = patientRepository.findPatientByPersonId(patientID).orElseThrow { throw EntityNotFoundException("Patient not found!") }
+
+        var score: Score? = null
+        if ((newScore.isPresent && newScore.get()) || patient.scores.isEmpty()) {
+            score = Score()
+            score.date = LocalDate.now()
+            score.patient = patient
+
+            patient.scores.add(score)
+        } else {
+            score = patient.scores.last()
+        }
+
+        score.values.add(scoreValue)
 
     }
 
--- src/main/kotlin/de/sepdetect/sepdetect/repository/PatientRepository.kt
+++ src/main/kotlin/de/sepdetect/sepdetect/repository/PatientRepository.kt
@@ -5,6 +5,7 @@ import de.sepdetect.sepdetect.model.Patient
 import org.springframework.data.jpa.repository.EntityGraph
 import org.springframework.data.jpa.repository.JpaRepository
 import java.time.LocalDate
+import java.util.*
 
 interface PatientRepository : JpaRepository<Patient, Long> {
     fun findAllByActive(active: Boolean): List<Patient>
@@ -12,7 +13,7 @@ interface PatientRepository : JpaRepository<Patient, Long> {
     fun findAllByActiveAndOrganizationIn(active: Boolean, organizations: List<Organization>): List<Patient>
 
     @EntityGraph(value = "graph.patient.scores.values")
-    fun findPatientByPersonId(id: Long): Patient
+    fun findPatientByPersonId(id: Long): Optional<Patient>
 
     fun findPatientByPersonLastNameContainingIgnoreCaseAndPersonSurnameContainingIgnoreCase(lastName: String, surname: String): List<Patient>
 
